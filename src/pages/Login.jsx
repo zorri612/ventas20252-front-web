3aaf38e0818d595b4340188304693769700bcbab
Karla Ceron <tu-correo@example.com>
2025-08-30 14:03:59 -0500

    login css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../services/api";
-import "../styles/login.css";
+import "../styles/Login.css";
 
 export default function Login() {
   const [email, setEmail] = useState("");
